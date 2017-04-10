52b84708cb892778a3581f72a73a7cfd04b34147
Chen Liu <Chen Liu>
2017-04-10 22:52:51 +0800

    add comments for VE's python script

--- ve.py
+++ ve.py
@@ -6,7 +6,7 @@ import os
 if not os.path.exists('output.xml'):
 	open('output.xml', 'w').close() 
 
-N = 1
+N = 1 # you can use one more loop to change N from 1 to 20
 tree = ElementTree()
 tree.parse('model0331.xml')
 root = tree.getroot()
@@ -16,7 +16,7 @@ for node in root.iter('node'):
 			if child1.tag.find('parameter') > -1:
 				if child1.attrib['name'].find('K') > -1:
 					oldvalue = child1.attrib['value']
-					newvalue = int(oldvalue) - 10*N
-					child1.attrib["value"] = str(newvalue)
+					newvalue = int(oldvalue) - 10*N #here need to change oldvalue from str to int
+					child1.attrib["value"] = str(newvalue) # here need to change int to str
 
 tree.write('output.xml')
