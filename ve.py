219b8405b7ca9f93380de561a0e803842c3b5632
Chen Liu <Chen Liu>
2017-04-10 22:48:23 +0800

    add VE's python script

---
+++ ve.py
@@ -0,0 +1,22 @@
+#-*- coding:utf-8 -*-
+
+#import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import ElementTree
+import os
+if not os.path.exists('output.xml'):
+	open('output.xml', 'w').close() 
+
+N = 1
+tree = ElementTree()
+tree.parse('model0331.xml')
+root = tree.getroot()
+for node in root.iter('node'):
+	if node.attrib.get('id').find('CatchmentCSS') > -1:
+		for child1 in node:
+			if child1.tag.find('parameter') > -1:
+				if child1.attrib['name'].find('K') > -1:
+					oldvalue = child1.attrib['value']
+					newvalue = int(oldvalue) - 10*N
+					child1.attrib["value"] = str(newvalue)
+
+tree.write('output.xml')
