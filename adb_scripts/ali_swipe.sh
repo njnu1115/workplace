9a4e6de848f6b3c58299e788ccb96fefae2bcdcd
Chen Liu <njnu1115@hotmail.com>
2024-08-07 21:38:02 +0800

    regular update

--- adb_scripts/ali_swipe.sh
+++ adb_scripts/ali_swipe.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 export PATH=/d/opt64/scrcpy:$PATH
 
-adb devices|grep UASNW19611006167 || adb connect 192.168.16.252:21218
-adb devices|grep 9c31ad62 || adb connect 192.168.16.102:5555 || adb connect 192.168.16.103:5555
+adb devices|grep UASNW19611006167 || adb connect 192.168.16.251:28218
+# adb devices|grep 9c31ad62 || adb connect 192.168.16.102:5555 || adb connect 192.168.16.103:5555
 
 android_devices=$(adb devices | awk '/^[^ ]+device$/ {print $1}')
 echo found android devices $android_devices
 
 # for device in $android_devices;
 # do
-#     scrcpy -s $device &
+#     scrcpy -s $device --encoder='OMX.google.h264.encoder' &
 # done
 
-for (( i=1; i<=50; i++ )); do
+# alipays://platformapi/startapp?appId=20000160
+
+for (( i=1; i<=100; i++ )); do
     echo "Starting iteration $i of 50..."
     for device in $android_devices;
     do
         if [ $((i % 5)) -eq 0 ]; then
             adb -s $device shell input keyevent 4
             sleep 1
-            adb -s $device shell input swipe 540 500 540 1000 100
+            adb -s $device shell input swipe 540 500 540 1600 89
             sleep 1
             adb -s $device shell input tap 900 1200
+            sleep 2
         fi
         SCREEN_WIDTH=$(adb -s $device shell wm size|awk -F"[ x\r]" '{print $3}')
         SCREEN_HEIGHT=$(adb -s $device shell wm size|awk -F"[ x\r]" '{print $4}')
