c3a50b27ddac2206e8f863d2b6424ec02979dc70
Chen Liu <njnu1115@gmail.com>
2024-11-15 11:46:07 +0800

    modify for xiaomi 10 umi

--- adb_scripts/ali_swipe.sh
+++ adb_scripts/ali_swipe.sh
@@ -10,16 +10,16 @@ echo "Current PATH: $PATH"
 
 # adb devices|grep UASNW19611006167 || adb connect 192.168.16.251:28218 || adb connect localhost:33333
 # adb devices|grep 9c31ad62 || adb connect 192.168.16.102:5555 || adb connect 192.168.16.103:5555
-adb devices|grep UASNW19611006167 || adb connect 192.168.16.251:28218 || adb connect localhost:33333
+adb devices|grep UASNW19611006167 || adb connect 192.168.16.134:33333 || adb connect localhost:33333
 
 adb devices|grep -w UASNW19611006167
 if [ $? -eq 0 ]; then
     android_devices=UASNW19611006167
 fi
 
-adb devices|grep -w '192.168.16.251:28218'
+adb devices|grep -w '192.168.16.134:33333'
 if [ $? -eq 0 ]; then
-    android_devices=192.168.16.251:28218
+    android_devices=192.168.16.134:33333
 fi
 
 adb devices|grep -w 'localhost:33333'
@@ -37,17 +37,18 @@ echo found android devices $android_devices
 
 # alipays://platformapi/startapp?appId=20000160
 
-for (( i=0; i<167; i++ )); do
-    echo "Starting iteration $i of 167..."
+for (( i=0; i<255; i++ )); do
+    echo "Starting iteration $i of 255..."
     for device in $android_devices;
     do
+	adb -s $device shell dumpsys power | grep "Display Power" | grep -q "state=OFF" && adb shell input keyevent 26
         if [ $((i % 7)) -eq 0 ]; then
             adb -s $device shell am start -d 'alipays://platformapi/startapp?appId=68687805\&url=https%3A%2F%2Frender.alipay.com%2Fp%2Fyuyan%2F180020380000000023%2Fpoint-sign-in.html'
-            sleep 16
+            sleep 4
             # adb -s $device shell input swipe 540 500 540 1600 89
             # sleep 1
-            adb -s $device shell input tap 900 1250
-            # adb -s $device shell input tap 900 1350
+            # for 2340x1080 screen
+	    adb -s $device shell input tap 900 1300
             sleep 8
         fi
         SCREEN_WIDTH=$(adb -s $device shell wm size|awk -F"[ x\r]" '{print $3}')
